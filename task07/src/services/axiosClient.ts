1acf629f23f691523a0341586b1bb88614465de7
Egor1025 <egor20051025@gmail.com>
2025-12-10 21:36:02 +0300

    Seminar 7 completed

---
+++ task07/src/services/axiosClient.ts
@@ -0,0 +1,14 @@
+import axios, { type AxiosRequestHeaders } from 'axios'
+
+export const axiosClient = axios.create()
+
+axiosClient.interceptors.request.use(config => {
+    config.baseURL = 'https://jsonplaceholder.typicode.com'
+    config.headers = {
+        ...(config.headers || {}),
+        Authorization: 'Bearer demo-token'
+    } as AxiosRequestHeaders
+    return config
+})
+
+export default axiosClient
---
+++ task07/src/services/usersApi.ts
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export interface User {
+    id?: number
+    name: string
+    email: string
+}
+
+export const usersApi = createApi({
+    reducerPath: 'usersApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://jsonplaceholder.typicode.com'
+    }),
+    tagTypes: ['Users'],
+    endpoints: builder => ({
+        getUsers: builder.query<User[], void>({
+            query: () => '/users',
+            providesTags: result =>
+                result
+                    ? [
+                        ...result.map(user => ({
+                            type: 'Users' as const,
+                            id: user.id
+                        })),
+                        { type: 'Users' as const, id: 'LIST' }
+                    ]
+                    : [{ type: 'Users' as const, id: 'LIST' }]
+        }),
+        addUser: builder.mutation<User, Omit<User, 'id'>>({
+            query: body => ({
+                url: '/users',
+                method: 'POST',
+                body
+            }),
+            invalidatesTags: [{ type: 'Users', id: 'LIST' }]
+        }),
+        updateUser: builder.mutation<User, User>({
+            query: user => ({
+                url: `/users/${user.id}`,
+                method: 'PUT',
+                body: user
+            }),
+            invalidatesTags: (_result, _error, arg) => [
+                { type: 'Users', id: arg.id },
+                { type: 'Users', id: 'LIST' }
+            ]
+        })
+    })
+})
+
+export const {
+    useGetUsersQuery,
+    useAddUserMutation,
+    useUpdateUserMutation
+} = usersApi
